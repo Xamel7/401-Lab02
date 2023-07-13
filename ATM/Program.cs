7f26c40e8d98a39d93a850244e9053b3af0ab14a
Xamel7 <joel.lewis47@yahoo.com>
2023-07-13 11:00:16 -0500

    ATM Init

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -1,10 +1,48 @@
 ﻿namespace ATM
 {
-    internal class Program
+    public class Program
     {
+        public static decimal balance = 0m;
+
+        public static decimal ViewBalance()
+        {
+            return balance;
+        }
+
+        public static decimal Withdraw(decimal amount)
+        {
+            if (balance - amount < 0)
+            {
+                return balance;
+            }
+            else
+            {
+                return balance -= amount;
+            }
+        }
+        public static decimal Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                return balance;
+            }
+            else
+            {
+                return balance + amount;
+            }
+        }
+        public static void UserInterface()
+        {
+            Console.WriteLine(ViewBalance());
+            Console.WriteLine(Deposit(20m));
+            Console.WriteLine(ViewBalance());
+            Console.WriteLine(Withdraw(15m));
+            Console.WriteLine(ViewBalance());
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello World Of Joe");
+            UserInterface();
         }
     }
 }
---
+++ ATM_Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ATM_Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
